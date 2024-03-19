2c0e2481606d89158580ffafb1155cfa9b0b7c40
Angeline Flora John <angelineflorajohn@icloud.com>
2024-03-19 05:23:10 -0700

    initial version

---
+++ movie-store-app/README.md
@@ -0,0 +1,10 @@
+# Movie store react application
+
+This is a simple react application making use of the following technologies:
+
+<ul>
+    <li>Bootstrap starter template</li>
+    <li>Font Awesome icons</li>
+    <li>Table view</li>
+    <li>Arrow function in js</li>
+</ul>
---
+++ movie-store-app/src/components/movies.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { getMovies } from "../otherServices/MovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    console.log("delete button clicked for movie " + movie);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Movies component</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
